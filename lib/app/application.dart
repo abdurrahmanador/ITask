db0c931c9c0ae9d2816f95ab8383b4ad8a12274a
abdr9 <abiabdullahinnshaalloh@gmail.com>
2024-06-20 01:11:02 +0600

    Initial Work has been started

---
+++ lib/app/application.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ITask extends StatelessWidget {
+  const ITask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
